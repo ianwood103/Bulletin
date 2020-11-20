9c7b4c44c4caa63cfb3ae738d0836891efec05ea
Ian Wood <ianwood103@gmail.com>
2020-11-20 13:17:10 -0500

    Added comments to all java classes

--- src/Bulletin/StartPage.java
+++ src/Bulletin/StartPage.java
@@ -16,11 +16,18 @@ public class StartPage extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        //Loads StartPage.fxml layout into root
         Parent root = FXMLLoader.load(getClass().getResource("StartPage.fxml"));
         window = primaryStage;
+
+        //Sets title of application window to be Bulletin
         window.setTitle("Bulletin");
 
+        //Creates new scene with root layout and width of 600 and height of 400
         Scene scene =  new Scene(root, 600, 400);
+
+        //Sets the scene of the window to the scene declared above and displays the window
         window.setScene(scene);
         window.show();
     }
