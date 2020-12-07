b3da423d530f952982bf60cdf434fbdac8dfabfa
Ian Wood <ianwood103@gmail.com>
2020-12-07 15:18:44 -0500

    Finished creating calendar button. Still has bug where image does not load when HomePage.fxml is reloaded

--- src/Bulletin/NewCalendarPageController.java
+++ src/Bulletin/NewCalendarPageController.java
@@ -6,24 +6,51 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.awt.*;
 import java.io.*;
+import java.nio.file.Files;
 
 public class NewCalendarPageController {
     public TextField nameField;
     public Button submitButton;
     public AnchorPane ap;
+    private File selectedFile;
 
-    public void submitButtonClick() throws IOException, ParseException {
+    public void imageButtonClick() throws IOException {
+        Stage window = (Stage) ap.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image File");
+        selectedFile = fileChooser.showOpenDialog(window);
+    }
+
+    public void submitButtonClick() throws IOException, ParseException, InterruptedException {
 
         //Creates a new directory for the new calendar that the user wants to create
         File file = new File("src/DefaultUser/" + nameField.getText() + "/");
         file.mkdir();
+
+        //Creates a new file name with logo as the name, but keeping the extension the same
+        int index = selectedFile.getName().lastIndexOf(".");
+        String ext = selectedFile.getName().substring(index);
+
+        //Stored in newFileName
+        String newFileName = "logo" + ext;
+
+        File newFile = new File(file.getPath() + "/" + newFileName);
+
+        //Copies the selected file into the project src folder, with newFileName as the new file name
+        Files.copy(selectedFile.toPath(), newFile.toPath());
+
+        if(newFile.exists() && !newFile.isDirectory()) {
+            System.out.println("file exists");
+        }
 
         //Parses existing list of calendars and stores that information in calendarList JSONArray
         JSONParser parser = new JSONParser();
@@ -33,7 +60,7 @@ public class NewCalendarPageController {
         //Creates a new JSONObject to hold the information for the new calendar
         JSONObject newCalendar = new JSONObject();
         newCalendar.put("name", nameField.getText());
-        newCalendar.put("image", "School/logo.png");
+        newCalendar.put("image", nameField.getText() + "/" + newFileName);
 
         //Adds new calendar JSONObject to JSONArray
         calendarList.add(newCalendar);
