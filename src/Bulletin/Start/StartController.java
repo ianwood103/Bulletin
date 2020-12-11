524d1d1c445a17879d3cfbbf5c45e6f628d58741
Ian Wood <ianwood103@gmail.com>
2020-12-11 13:07:52 -0500

    made changes to login page

--- src/Bulletin/Start/StartController.java
+++ src/Bulletin/Start/StartController.java
@@ -3,16 +3,22 @@ package Bulletin.Start;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 //Controller class for Start.fxml
 public class StartController {
 
-    //Declares variables for each text field and also the anchorpane that contains the entire layout
+    //Declares variables for each text field and also the AnchorPane that contains the entire layout
+    public VBox outerBox;
+    public VBox innerBox;
     public TextField nameInput;
-    public TextField passInput;
+    public PasswordField passInput;
     public AnchorPane ap;
 
     public void login() throws Exception {
@@ -24,7 +30,7 @@ public class StartController {
         //Loads the layout from Home.fxml into homePage
         Parent homePage = FXMLLoader.load(getClass().getResource("../Home/Home.fxml"));
 
-        //Using the declared anchorpane from above, this stores the current window in the window variable
+        //Using the declared AnchorPane from above, this stores the current window in the window variable
         Stage window = (Stage) ap.getScene().getWindow();
 
         //Creates a new scene to have the layout from Home.fxml and keeps the window size the same
@@ -32,5 +38,23 @@ public class StartController {
 
         //Changes the scene to this newly created scene
         window.setScene(scene);
+    }
+
+    public void loginClick() {
+        outerBox.toBack();
+        outerBox.setStyle("-fx-opacity: 0");
+        innerBox.setStyle("-fx-opacity: 1");
+    }
+
+    public void initialize() {
+        passInput.addEventHandler(KeyEvent.KEY_PRESSED, ev -> {
+            if (ev.getCode() == KeyCode.ENTER) {
+                try {
+                    this.login();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
