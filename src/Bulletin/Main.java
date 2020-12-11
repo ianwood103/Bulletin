020f8796568ef45943645624c0eb29aba9fa2e38
Ian Wood <ianwood103@gmail.com>
2020-12-11 13:16:33 -0500

    updated login page

--- src/Bulletin/Main.java
+++ src/Bulletin/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
         window.setTitle("Bulletin");
 
         //Creates new scene with root layout and width of 600 and height of 400
-        Scene scene =  new Scene(root, 600, 400);
+        Scene scene =  new Scene(root);
 
         //Sets the scene of the window to the scene declared above and displays the window
         window.setScene(scene);
