0322aeab6dd2fec87788a9adb2b44500e05787c8
Ian Wood <ianwood103@gmail.com>
2020-12-14 02:56:45 -0500

    made a few more minor tweaks to ui

--- src/Bulletin/Start/StartController.java
+++ src/Bulletin/Start/StartController.java
@@ -1,13 +1,16 @@
 package Bulletin.Start;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,10 +19,13 @@ public class StartController {
 
     //Declares variables for each text field and also the AnchorPane that contains the entire layout
     public VBox outerBox;
+    public VBox middleBox;
     public VBox innerBox;
     public TextField nameInput;
     public PasswordField passInput;
     public AnchorPane ap;
+    public BorderPane exitPane;
+    public Button exitButton;
 
     public void login() throws Exception {
 
@@ -42,8 +48,42 @@ public class StartController {
 
     public void loginClick() {
         outerBox.toBack();
+        middleBox.toBack();
         outerBox.setStyle("-fx-opacity: 0");
         innerBox.setStyle("-fx-opacity: 1");
+        exitPane.setStyle("-fx-opacity: 1");
+        exitButton.setOnAction(e -> {
+            try {
+                this.exitClick();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        exitButton.setStyle("-fx-cursor: hand");
+    }
+
+    public void createAccountClick() {
+        outerBox.toBack();
+        innerBox.toBack();
+        middleBox.setStyle("-fx-opacity: 1");
+        innerBox.setStyle("-fx-opacity: 0");
+        outerBox.setStyle("-fx-opacity: 0");
+        exitPane.setStyle("-fx-opacity: 1");
+        exitButton.setOnAction(e -> {
+            try {
+                this.exitClick();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        exitButton.setStyle("-fx-cursor: hand");
+    }
+
+    public void exitClick() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Start.fxml"));
+        Parent root = loader.load();
+        Stage window = (Stage) ap.getScene().getWindow();
+        window.setScene(new Scene(root, window.getWidth(), window.getHeight() - 28));
     }
 
     public void initialize() {
